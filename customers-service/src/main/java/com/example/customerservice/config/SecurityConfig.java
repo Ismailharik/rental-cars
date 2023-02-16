254386a080ef6b7434f100014532b5e7ad35731a
ismail harik <92827404+Ismailharik@users.noreply.github.com>
2023-02-16 12:48:48 +0100

    add monitoring ( grafana , prometheus )

--- customers-service/src/main/java/com/example/customerservice/config/SecurityConfig.java
+++ customers-service/src/main/java/com/example/customerservice/config/SecurityConfig.java
@@ -16,7 +16,8 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/actuator/**").permitAll().anyRequest().authenticated()
+                        .requestMatchers("/actuator/**").permitAll().
+                        anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
         return http.build();
